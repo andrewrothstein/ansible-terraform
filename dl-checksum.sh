9753307eb159dcf44753800ae178041940d7ba67
Andrew Rothstein <andrew.rothstein@gmail.com>
2019-12-03 18:35:02 -0500

    up to v0.12.17

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -1,42 +1,47 @@
 #!/usr/bin/env sh
 APP=terraform
-VER=${1:-0.12.16}
 DIR=~/Downloads
-MIRROR=https://releases.hashicorp.com/${APP}/${VER}
-
-CHECKSUMS=${APP}_${VER}_SHA256SUMS
-LCHECKSUMS=$DIR/$CHECKSUMS
-RCHECKSUMS=$MIRROR/$CHECKSUMS
-
-if [ ! -e $DIR/$CHECKSUMS ];
-then
-    wget -q -O $LCHECKSUMS $RCHECKSUMS
-fi
-
+MIRROR=https://releases.hashicorp.com/${APP}
 
 dl()
 {
-    OS=$1
-    ARCH=$2
-    PLATFORM=${OS}_${ARCH}
-    FILE=${APP}_${VER}_${PLATFORM}.zip
-    URL=$MIRROR/$FILE
+    local ver=$1
+    local lchecksums=$2
+    local os=$3
+    local arch=$4
+    local platform=${os}_${arch}
+    local file=${APP}_${ver}_${platform}.zip
+    local url=$MIRROR/$ver/$file
 
-    printf "    # %s\n" $URL
-    printf "    %s: sha256:%s\n" $PLATFORM `fgrep $FILE $LCHECKSUMS | awk '{print $1}'`
+    printf "    # %s\n" $url
+    printf "    %s: sha256:%s\n" $platform `fgrep $file $lchecksums | awk '{print $1}'`
 }
 
-printf "  # %s\n" $RCHECKSUMS
-printf "  '%s':\n" $VER
-dl darwin amd64
-dl freebsd 386
-dl freebsd amd64
-dl freebsd arm
-dl linux 386
-dl linux amd64
-dl linux arm
-dl openbsd 386
-dl openbsd amd64
-dl solaris amd64
-dl windows 386
-dl windows amd64
+dl_all () {
+    local ver=$1
+    local checksums=${APP}_${ver}_SHA256SUMS
+    local lchecksums=$DIR/$checksums
+    local rchecksums=$MIRROR/$ver/$checksums
+
+    if [ ! -e $lchecksums ];
+    then
+        wget -q -O $lchecksums $rchecksums
+    fi
+
+    printf "  # %s\n" $rchecksums
+    printf "  '%s':\n" $ver
+    dl $ver $lchecksums darwin amd64
+    dl $ver $lchecksums freebsd 386
+    dl $ver $lchecksums freebsd amd64
+    dl $ver $lchecksums freebsd arm
+    dl $ver $lchecksums linux 386
+    dl $ver $lchecksums linux amd64
+    dl $ver $lchecksums linux arm
+    dl $ver $lchecksums openbsd 386
+    dl $ver $lchecksums openbsd amd64
+    dl $ver $lchecksums solaris amd64
+    dl $ver $lchecksums windows 386
+    dl $ver $lchecksums windows amd64
+}
+
+dl_all  ${1:-0.12.17}
